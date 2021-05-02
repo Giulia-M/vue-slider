61a8da1ac85b4db9e03232d1955daafa5f54be2d
Giulia-M <giuliamaggio16@gmail.com>
2021-05-02 22:13:32 +0200

    slider with dots

--- myScript.js
+++ myScript.js
@@ -1,15 +1,57 @@
-const app = new Vue (
+/*
+Creare uno slider di immagini
+usate immagini delle stesse dimensioni 
+
+Lo slider prevederà due frecce (icone) che permettono di scorrere tra le immagini dello slider 
+
+Inoltre per scorrere le immagini permettere anche l’uso delle frecce sinistra e destra della tastiera.
+
+Utiliziamo una classe first e last per capire quali sono la prima e ultima immagine dello slider 
+
+Utilizziamo una classe active per aiutarci a capire quale è l’immagine attuale da visualizzare nello slider
+
+Bonus: Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+*/
+const app = new Vue(
     {
         el: '#app',
+
         data: {
-            impostaImg: [
-                {
-                    
-                }
-            ]
+            timer: null,
+            characters: [
+                "img/foto1.jpg",
+                "img/foto2.jpg",
+                "img/foto3.jpg",
+                "img/foto4.jpg",
+                "img/foto5.jpg"
+            ],
+            activeImage: 0,
+            slides: 5,
         },
+        mounted: function () {
+            this.startSlider()
+        },
+
         methods: {
-            
+            startSlider: function () {
+                this.timer = setInterval(this.next, 4000)
+
+            },
+
+            currentImg: function () {
+                return this.characters[Math.abs(this.activeImage) % this.characters.length];
+            },
+            next: function () {
+                this.activeImage += 1;
+            },
+            prev: function () {
+                this.activeImage -= 1;
+            },
+
+            jump(index) {
+                this.activeImage = index
+            }
+
         }
     }
 )
